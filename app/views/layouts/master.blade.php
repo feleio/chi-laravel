8ead6e53dfbf58c605ca1999d1d93349aba33f50
felix <lokling@gmail.com>
2014-11-19 18:19:44 +1100

    added google analytics js

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -13,12 +13,24 @@
         {{ HTML::style('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css')}}
         @yield('head-css')
         @yield('head-js')
+        @if(!App::isLocal())
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-56421171-1', 'auto');
+          ga('send', 'pageview');
+        </script>
+        @endif
     </head>
 
     <body>
         <div class="c-admin c-center">
             @if (Auth::check())
             <div class="f_right">
+                <span>env: {{app('env')}}</span>
                 <span>{{$mailsCount}} mails</span>
                 <span>{{$requestsCount}} postcard</span>
                 <a class="btn btn-primary btn-xs" href="{{URL::to('posts/create')}}">
