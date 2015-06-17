4ffc1c375579066a54aedbf601399ef593b773cd
felix <lokling@gmail.com>
2015-06-18 00:11:03 +0800

    fix git server

--- .rocketeer/scm.php
+++ .rocketeer/scm.php
@@ -8,13 +8,13 @@
 
 	// The SSH/HTTPS address to your repository
 	// Example: https://github.com/vendor/website.git
-	'repository' => '/home/vagrant/Git/chi.git',
+	'repository' => 'ssh://git@ec2-52-68-44-226.ap-northeast-1.compute.amazonaws.com:/opt/git/chi.git',
 
 	// The repository credentials : you can leave those empty
 	// if you're using SSH or if your repository is public
 	// In other cases you can leave this empty too, and you will
 	// be prompted for the credentials on deploy
-	'username'   => '',
+	'username'   => 'git',
 	'password'   => '',
 
 	// The branch to deploy
