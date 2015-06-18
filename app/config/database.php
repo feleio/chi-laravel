4ca16f88f7074e58906403f5eeb37503e5cc8bbf
felix <lokling@gmail.com>
2015-06-18 22:30:32 +0800

    change db setting

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'chisincheese.com',
 			'username'  => 'root',
-			'password'  => 'zxadzxad',
+			'password'  => 'Zxadzxad1001',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
